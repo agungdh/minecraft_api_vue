c5118c3186675cb77ac9725f45d7a99641388988
AgungDH <agungdh@live.com>
2020-12-31 19:01:25 +0700

    asfasf

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-main>
       <Header/>
       <HelloWorld/>
+      <Link/>
     </v-main>
   </v-app>
 </template>
@@ -10,6 +11,7 @@
 <script>
 import HelloWorld from './components/HelloWorld';
 import Header from './components/Header';
+import Link from './components/Link';
 
 export default {
   name: 'App',
@@ -17,6 +19,7 @@ export default {
   components: {
     HelloWorld,
     Header,
+    Link,
   },
 
   data: () => ({
---
+++ src/components/Link.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card>
+    <v-list-item>
+      <v-list-item-content>
+        <div class="text-center">
+            <v-tooltip top>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                    class="mx-2"
+                    fab
+                    dark
+                    large
+                    color="cyan"
+                    v-bind="attrs"
+                    v-on="on"
+                    v-on:click="telegram"
+                    >
+                        <v-icon>
+                            mdi-telegram
+                        </v-icon>
+                    </v-btn>
+                </template>
+                <span>Telegram</span>
+            </v-tooltip>
+        </div>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Link',
+    methods: {
+        telegram() {
+            window.open('https://t.me/EgaNoobMinecraftServerWatcher', '_blank');
+        }
+    }
+  }
+</script>
